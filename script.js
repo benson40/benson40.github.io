ad528f84ac05d2dbe49e1c69ae230638aedbabf5
Benson Samuel Jose <56859107+benson40@users.noreply.github.com>
2021-03-15 00:07:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Karen", "Jacob", "Johnson", "Ross", "Paul", "Jason", "Neil", "David", "Lauren", "Jamy"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
