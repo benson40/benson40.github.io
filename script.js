b7d5aae511f16e234e78c61631bdad9c24708e14
Benson Samuel Jose <56859107+benson40@users.noreply.github.com>
2021-03-14 22:07:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 (function() {
     var names = ["Karen", "Jacob", "Johnson", "Ross", "Paul", "Jason", "Neil", "David", "Lauren", "Jamy"];
     for (var i = 0; i < names.length; i++) {
-        var firstletter = names[i].charAt(0).toLowerCase();
-        if (firstletter === 'j') {
-            byespeaker.speak(names[i]);
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
         } else {
-            hellospeaker.speak(names[i]);
+            helloSpeaker.speak(names[i]);
         }
     }
 })();
